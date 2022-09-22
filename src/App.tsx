2a17b4d00cc9c881a7a119f819dcee2a5504a712
Alexey Zerkalenkov <37899988+a-zerkalenkov@users.noreply.github.com>
2022-09-22 22:40:37 +0200

    Load chat flow json async

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
 
-function App() {
-  return (
+export type ValueOption = {
+  nextId: number;
+  value: boolean;
+  text: string;
+}
+
+export type FlowItem = {
+  id: number;
+  name: string;
+  text: string;
+  uiType: string;
+  valueType: boolean;
+  valueOptions: ValueOption[];
+}
+
+interface AppStateProps {
+  isLoading: boolean;
+  flow: FlowItem[];
+  error: boolean;
+}
+
+const initialState: AppStateProps = {
+  isLoading: true,
+  flow: [],
+  error: false
+}
+
+const App: React.FC = () => {
+  const [state, setState] = React.useState(initialState);
+
+  fetch('https://raw.githubusercontent.com/mzronek/task/main/flow.json')
+    .then((response) => response.json()
+        .then((flow) => setState({
+            ...state,
+            isLoading: false,
+            flow
+          })
+        ),
+      (error) => setState({
+        ...state,
+        error: true
+      }));
+
+  return state.error ? <div>ERROR</div> : state.isLoading ? <div>Loading...</div> : (
     <div>
-      Here goes content
+      {state.flow.map((item) => {
+        return (
+          <div>
+            {item.text}
+          </div>
+        )
+      })}
     </div>
   );
 }
